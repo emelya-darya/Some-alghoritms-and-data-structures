5d90ecbe3a16790bab39d25c2980d6c40bf956e5
Darya <emelyadarya@yandex.ru>
2023-12-13 00:28:01 +0500

    add dfs

---
+++ src/Algorithms/Graphs/DFS/DepthFirstSearch.js
@@ -0,0 +1,72 @@
+import { Stack } from './../../../DataStructures/Stack/Stack.js'
+
+/*
+  if reachableFrom - is a string with the name of the vertex, then a list of vertices reachable from the given one will be returned
+  else if reachableFrom === null - will return a list of all reachable vertices of the graph
+*/
+export const dfs = function (graph, reachableFrom = null) {
+   const vrtxsStack = new Stack()
+   const reachableVrtxs = {}
+
+   if (reachableFrom === null) {
+      for (const vrtx in graph) {
+         vrtxsStack.put(vrtx)
+         reachableVrtxs[vrtx] = true
+      }
+   } else {
+      vrtxsStack.put(reachableFrom)
+      reachableVrtxs[reachableFrom] = true
+   }
+
+   while (!vrtxsStack.isEmpty()) {
+      const currVrtx = vrtxsStack.extract() || ''
+      const currVrtxsNeighbours = graph[currVrtx]
+
+      if (Array.isArray(currVrtxsNeighbours)) {
+         currVrtxsNeighbours.forEach(nb => {
+            if (!reachableVrtxs[nb]) {
+               reachableVrtxs[nb] = true
+               vrtxsStack.put(nb)
+            }
+         })
+      }
+   }
+
+   const output = []
+   for (const vrtx in reachableVrtxs) output.push(vrtx)
+
+   return output
+}
+
+/*
+ ? **************************************************************************************************************************
+ ? **************************************************************************************************************************
+ ? **************************************************************************************************************************
+ ? ************************************************* Usage examples *********************************************************
+ ? **************************************************************************************************************************
+ ? **************************************************************************************************************************
+ ? **************************************************************************************************************************
+ ? **************************************************************************************************************************
+ */
+
+const testGraph = {
+   a: ['b', 'c'],
+   b: ['f'],
+   c: ['d', 'e'],
+   d: ['f'],
+   e: ['f'],
+   f: ['g'],
+}
+
+/*
+     console.log(dfs(testGraph))
+ 
+    *Result 
+    [ 'a', 'b', 'c', 'd', 'e', 'f', 'g' ]
+ 
+    !=============================================================================================
+ 
+    console.log(dfs(testGraph, 'c'))
+    *Result  
+    [ 'c', 'd', 'e', 'f', 'g' ]
+ */
